9fa10b585749315cc93e0cc7aace7ec71be1dce2
Lincoln Wallace V. Almeida <lincolnble@hotmail.com>
2021-03-21 14:31:16 -0300

    In this branch, I will try to use Firefox browser instead Chrome browser

---
+++ webscraping_yahoo_finances_firefox_version.py
@@ -0,0 +1,21 @@
+"""
+Webscraping from yahoo finances
+
+Author: Lincoln Wallace
+date: 07/03/2021
+"""
+
+# Begin Imports section
+
+import time
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+import json
+
+# End Import section
+
+
